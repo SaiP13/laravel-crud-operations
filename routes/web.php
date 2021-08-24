60a6277e257f0ce6861a546c671b01c20589df21
SaiP13 <chsai.p13@gmail.com>
2021-08-24 14:07:00 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/addUser', 'HomeController@addUser')->name('addUser');
 Route::post('/addNewUser', 'HomeController@addNewUser')->name('addNewUser');
 Route::get('deleteUser/{id}', 'HomeController@deleteUser')->name('deleteUser');
 Route::get('editUser/{id}', 'HomeController@editUser')->name('editUser');
-Route::post('updateUser/{id}', 'HomeController@updateUser')->name('updateUser');
+Route::post('updateUser/{id}', 'HomeController@updateUser')->name('updateUser')->middleware('check');
 
 //artcles
 Route::resource('articles', 'ArticleController');
